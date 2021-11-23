ad3ed3b1dadaa0508733d130c876bb915a59909f
Kin Lam <lamhokkin@cherrypicks.com>
2021-11-23 17:24:37 +0800

    no message

--- js/script.js
+++ js/script.js
@@ -1,33 +1,78 @@
-var video = document.querySelector("#cam");
-var mediaConfig =  { video: true };
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
-        // video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
-    }, errBack);
-}
+$(document).ready(function(){
+    var video = document.querySelector("#cam");
+    var mediaConfig =  { video: true };
+    var current_camera = "";
+    if($("#cam").length){
+        setCamera(mediaConfig);
+    }
+    
 
-else if(navigator.getUserMedia) { // Standard
-    navigator.getUserMedia(mediaConfig, function(stream) {
-        video.src = stream;
-        video.play();
-    }, errBack);
-} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-    navigator.webkitGetUserMedia(mediaConfig, function(stream){
-        video.src = window.webkitURL.createObjectURL(stream);
-        video.play();
-    }, errBack);
-} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
-    navigator.mozGetUserMedia(mediaConfig, function(stream){
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-    }, errBack);
-}else{
-    //video.src = base_url+"/video/test_mp4.mp4";
-}
+    function setCamera(mediaConfig){
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
+                // video.src = window.URL.createObjectURL(stream);
+                video.srcObject = stream;
+                video.play();
+            }, errBack);
+        }
 
-var errBack = function(e) {
-    console.log('An error has occurred!', e)
+        else if(navigator.getUserMedia) { // Standard
+            navigator.getUserMedia(mediaConfig, function(stream) {
+                video.src = stream;
+                video.play();
+            }, errBack);
+        } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+            navigator.webkitGetUserMedia(mediaConfig, function(stream){
+                video.src = window.webkitURL.createObjectURL(stream);
+                video.play();
+            }, errBack);
+        } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+            navigator.mozGetUserMedia(mediaConfig, function(stream){
+                video.src = window.URL.createObjectURL(stream);
+                video.play();
+            }, errBack);
+        }else{
+            //video.src = base_url+"/video/test_mp4.mp4";
+        }
+    }
+    function errBack(){
+        console.log("error")
+    }
 
-};
+    $(".switch-camera").click(function(){
+        if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+            console.log("enumerateDevices is not supported.");
+        }else{
+            navigator.mediaDevices.enumerateDevices().then((devices) => {
+               
+                
+                devices.forEach((device) => {
+                    let option = new Option();
+                    option.value = device.deviceId;
+            
+                    // According to the type of media device
+                    switch(device.kind){
+                        // Append device to list of Cameras
+                        case "videoinput":
+                            if(device.deviceId != current_camera){
+                                console.log("select " + device);
+                                mediaConfig.video = {
+                                    deviceId: device.deviceId
+                                }
+                                navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
+                                    // video.src = window.URL.createObjectURL(stream);
+                                    video.srcObject = stream;
+                                    video.play();
+                                }, errBack);
+                            }
+                            break;
+                    }
+            
+                    console.log(device);
+                });
+            }).catch(function (e) {
+                console.log(e.name + ": " + e.message);
+            });
+        }
+    })
+});
