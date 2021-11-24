81da8c965d1e2a2b8cded29b1e82bff4d6d5ba88
Kin Lam <lamhokkin@cherrypicks.com>
2021-11-24 16:42:18 +0800

    no message

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
    
     var current_camera = "";
     var facingMode = "environment";
-    var mediaConfig =  { video: { facingMode:  facingMode} };
+    var mediaConfig =  {
+        video: {
+            width: 1280,
+            height: 720,
+            facingMode:  facingMode
+        }
+    };
     if($("#cam").length){
         setCamera(mediaConfig);
     }
@@ -16,30 +22,30 @@ $(document).ready(function(){
                 video.srcObject = null;
                 video.srcObject = stream;
                 video.play();
-            }, errBack);
+            }, errBack());
         }
 
         else if(navigator.getUserMedia) { // Standard
             navigator.getUserMedia(mediaConfig, function(stream) {
                 video.src = stream;
                 video.play();
-            }, errBack);
+            }, errBack());
         } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
             navigator.webkitGetUserMedia(mediaConfig, function(stream){
                 video.src = window.webkitURL.createObjectURL(stream);
                 video.play();
-            }, errBack);
+            }, errBack());
         } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
             navigator.mozGetUserMedia(mediaConfig, function(stream){
                 video.src = window.URL.createObjectURL(stream);
                 video.play();
-            }, errBack);
+            }, errBack());
         }else{
             //video.src = base_url+"/video/test_mp4.mp4";
         }
     }
     function errBack(){
-        console.log("error")
+        console.log("errBack")
     }
     if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
         console.log("enumerateDevices is not supported.");
