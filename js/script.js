0ed20f26020933a85a40369aa4a04c86c4a7e895
Kin Lam <lamhokkin@cherrypicks.com>
2021-12-06 04:14:52 +0800

    no message

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
                             },
                             success: function(data) {
                                 console.log(data['Key']);
-                                // window.location = "./share.html#"+data['Key']
+                                window.location = "./share.html#"+data['Key']
                             }
                         })
                     })
